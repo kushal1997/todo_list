7ac0e075bb053533f8cbff07a75f31ba6964e7b6
kushal1997 <kushalrao103@gmail.com>
2024-01-15 08:34:10 +0530

    basic file structure is react

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const port = 8000;
+const app=express();
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Error is ${err}`)
+    }
+    console.log(`Server is running on port: ${port}`)
+})
