31f51dc1cd63378efeb136f3a9ad27a5b16a6139
kushal1997 <kushalrao103@gmail.com>
2024-01-15 08:43:02 +0530

    start up controller is ready for home page

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express=require('express');
 const port = 8000;
 const app=express();
 
+app.use('/',require('./routes'))
+
+app.set('view engine','ejs')
+app.set('views','./views')
+
 app.listen(port,(err)=>{
     if(err){
         console.log(`Error is ${err}`)
